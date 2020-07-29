ec8b7b90a1487c9164a9dc125038ba10a00b6c27
86096 <zhengxinfei@inrong.com.cn>
2020-07-29 10:01:07 +0800

    点击图标 弹出企业信息 跳转到企业

--- bootdo/src/main/java/com/bootdo/ecosys/controller/FiredeviceController.java
+++ bootdo/src/main/java/com/bootdo/ecosys/controller/FiredeviceController.java
@@ -10,15 +10,13 @@ import com.bootdo.ecosys.domain.FiredeviceDO;
 import com.bootdo.ecosys.service.CodeService;
 import com.bootdo.ecosys.service.EnterpriseService;
 import com.bootdo.ecosys.service.FiredeviceService;
-import com.sun.xml.internal.bind.v2.TODO;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
